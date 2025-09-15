b55f42d57b2dcb1c3e4adb3c1e90559dc5031546
evtish <evevtish@gmail.com>
2025-09-15 20:37:35 +0300

    Update circle

--- main.c
+++ main.c
@@ -8,19 +8,21 @@
 #define FONT_H 27//15
 
 bool insideCircle(int x, int y, int a, int b, int R) {
+    // printf("%d\n", (x - a) * (x - a) + (y - b) * (y - b) < h * k);
     return (x - a) * (x - a) + (y - b) * (y - b) < R * R;
 }
 
 int main(void) {
-    // double screenRatio = (double) FONT_H / FONT_W;
-    // int CLI_W = CLI_H * screenRatio;
+    double screenRatio = (double) FONT_H / FONT_W;
+    int cliW = CLI_H * screenRatio, h = CLI_H / 2, k = cliW / 2;
     // int rectH = CLI_H, rectW = rectH * screenRatio;
     
-    // printf("screenRatio = %lf, rectW = %d\n", screenRatio, rectW);
+    // printf("screenRatio = %lf, h = %d, k = %d\n", screenRatio, h, k);
 
     for (int i = 0; i < CLI_H; i++) {
-        for (int j = 0; j < CLI_H; j++) {
-            printf("%c", (insideCircle(i, j, CLI_H / 2, CLI_H / 2, CLI_H / 2)) ? '#' : ' ');
+        for (int j = 0; j < cliW; j++) {
+            // insideCircle(j, i, k, h, h, k);
+            printf("%c", (insideCircle(j, i, k, h, h)) ? '#' : ' ');
         }
         printf("\n");
     }
